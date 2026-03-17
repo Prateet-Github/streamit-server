1e70ffb1a17c8b24201c7218ec816e9a06e0ecea
Prateet Tiwari <prateettiwari29@gmail.com>
2026-03-17 14:18:30 +0530

    feat: add authentication middleware to upload URL route

--- routes/video-url.route.js
+++ routes/video-url.route.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { getPreSignedUrl } from "../controllers/video-url.controller.js";
+import {protect} from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
-router.post('/upload-url', getPreSignedUrl);
+router.post('/upload-url', protect , getPreSignedUrl);
 
 export default router;
